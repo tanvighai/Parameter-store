ab658bc3ef0284e78395eb823b30c9d00bc74cc7
tanvighai <tanvighai02@gmail.com>
2023-12-21 00:20:57 -0500

    test commit

--- main.tf
+++ main.tf
@@ -10,7 +10,13 @@ variable "parameters" {
     {name = "prod.rds.master_username" , value ="mysqlrdsadmin" , type = "String"},
     {name = "prod.expense.frontend.backend_url" , value ="http://backend-prod.tanvi12online.net/" , type = "String"} ,
     {name = "prod.rds.endpoint" , value ="prod-mysql.cluster-c1m20yogsjyg.us-east-1.rds.amazonaws.com" , type = "String"} ,
-    {name = "dev.expense.frontend.backend_url" , value ="http://backend-dev.tanvi12online.net/" , type = "String"}
+    {name = "dev.expense.frontend.backend_url" , value ="http://backend-dev.tanvi12online.net/" , type = "String"} ,
+
+
+##common env
+
+    {name = "jenkins_password" , value ="Admin123" , type = "SecureString"} ,
+
 
 
   ]
